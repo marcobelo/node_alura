9bb345ce301a709733f601995f046e370d9e0fff
marcobelo <marco.barone.belo@gmail.com>
2020-12-11 13:00:44 -0300

    first route

--- course_1/code/index.js
+++ course_1/code/index.js
@@ -3,3 +3,5 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, () => console.log('servidor rodando na 3000'))
+
+app.get('/', (request, response) => response.send('Servidor rodando, ok!'))
