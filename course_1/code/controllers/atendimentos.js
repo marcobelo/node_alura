90304d7b187efc497732d493f496b80e396f594d
marcobelo <marco.barone.belo@gmail.com>
2020-12-19 09:02:51 -0300

    add body parser

--- course_1/code/controllers/atendimentos.js
+++ course_1/code/controllers/atendimentos.js
@@ -1,3 +1,8 @@
 module.exports = app => {
-    app.get('/atendimentos', (request, response) => response.send('Servidor rodando, ok!'))
+    app.get('/atendimentos', (request, response) => response.send('Você está na rota de atendimentos e está realizando um GET'))
+
+    app.post('/atendimentos', (request, response) => {
+        console.log(request.body)
+        response.send('Você está na rota de atendimentos e está realizando um POST')
+    })
 }
