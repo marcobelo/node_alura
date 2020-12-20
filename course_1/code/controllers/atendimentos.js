c3f543ca7f8d04c58f8495b21d195d1cd0a02edc
marcobelo <marco.barone.belo@gmail.com>
2020-12-20 11:42:08 -0300

    route atendimento now save data on database

--- course_1/code/controllers/atendimentos.js
+++ course_1/code/controllers/atendimentos.js
@@ -1,8 +1,11 @@
+const atendimento_model = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (request, response) => response.send('Você está na rota de atendimentos e está realizando um GET'))
 
     app.post('/atendimentos', (request, response) => {
-        console.log(request.body)
-        response.send('Você está na rota de atendimentos e está realizando um POST')
+        const atendimento = request.body
+        atendimento_model.adiciona(atendimento)
+        response.send('Atendimento salvo')
     })
 }
---
+++ course_1/code/models/atendimentos.js
@@ -0,0 +1,17 @@
+const connection = require('../infrastructure/connection')
+
+
+class Atendimento {
+    adiciona(atendimento) {
+        const sql = 'INSERT INTO Atendimentos SET ?'
+        connection.query(sql, atendimento, (erro, resultados) => {
+            if (erro) {
+                console.log(erro);
+            } else {
+                console.log(resultados);
+            }
+        })
+    }
+}
+
+module.exports = new Atendimento
