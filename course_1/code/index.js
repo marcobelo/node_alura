8d049911fdde7518aa611d6a9749317d27346abb
marcobelo <marco.barone.belo@gmail.com>
2020-12-20 11:30:16 -0300

    creating table Atendimentos

--- course_1/code/index.js
+++ course_1/code/index.js
@@ -1,12 +1,13 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infrastructure/connection')
+const Tables = require('./infrastructure/tables')
 
 conexao.connect(erro => {
     if (erro) {
         console.log(erro);
     } else {
         console.log('conectado com sucesso ao banco');
-
+        Tables.init(conexao)
         const app = customExpress()
 
         app.listen(3000, () => console.log('servidor rodando na 3000'))
---
+++ course_1/code/infrastructure/tables.js
@@ -0,0 +1,22 @@
+const connexao = require("./connection");
+
+class Tables {
+    init(connection) {
+        this.connection = connection
+        this.criarAtendimentos()
+    }
+    criarAtendimentos() {
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+        this.connection.query(sql, (erro) => {
+            if (erro) {
+                console.log(erro);
+            } else {
+                console.log('Tabela Atendimentos criada');
+            }
+        }
+
+        )
+    }
+}
+
+module.exports = new Tables
